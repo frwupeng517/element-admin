ac2f9fab2bdecf3123bcb6b8a18006a535805de6
wupeng <710186001@qq.com>
2018-08-27 17:30:50 +0800

    回到页面顶部

--- src/views/announce/bulletin/List.vue
+++ src/views/announce/bulletin/List.vue
@@ -1,3 +1,130 @@
 <template>
-  <h1>公告列表</h1>
+  <div>
+    <h1>返回顶部</h1>
+    <div>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+      <el-alert title="山桃红花满上头，蜀江春水拍山流。" type="success"></el-alert>
+    </div>
+    <el-tooltip placement="top" content="返回顶部">
+      <back-to-top 
+        transitionName="fade"
+        :customStyle="myBackToTopStyle" 
+        :visibilityHeight="300" 
+        :backPosition="50">
+      </back-to-top>
+    </el-tooltip>
+  </div>
 </template>
+
+<script>
+import BackToTop from '@/components/BackToTop'
+
+export default {
+  data() {
+    return {
+      myBackToTopStyle: {
+        'right': '100px',
+        'bottom': '150px',
+        'width': '40px',
+        'height': '40px',
+        'border-radius': '20px',
+        'line-height': '40px', 
+        'background': '#fff'
+      }
+    }
+  },
+  components: {
+    BackToTop
+  }
+}
+</script>
+
